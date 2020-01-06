5e1e5ee88786a639df55c737c61bf9a13a646167
“Kareem <kareembyrd@gmail.com>
2020-01-06 17:35:52 +0000

    Updated Lesson 15

--- src/App.js
+++ src/App.js
@@ -27,16 +27,22 @@ getNotes = () => {
   .catch((err) => console.log(err.response.data) );
 }
 
-  render() {
-    const { showNote } = this.state;
+render() {
+  const { showNote, notes } = this.state;
 
-    return (
-      <div className="App">
-        <Nav toggleNote={this.toggleNote} showNote={showNote} />
-        { showNote ? <Note /> : <List getNotes={this.getNotes}/> }
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Nav toggleNote={this.toggleNote} showNote={showNote} />
+      {showNote ?
+        <Note />
+        :
+        <List
+          getNotes={this.getNotes}
+          notes={notes}
+        />
+      }
+    </div>
+  );
 }
 
 export default App;
